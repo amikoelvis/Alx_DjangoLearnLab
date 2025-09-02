7c841d3867f920910871840dc47801a007bc432a
Amiko Elvis <amikoelvis@gmail.com>
2025-09-02 16:35:02 +0300

    jdhfhfh

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("librarian-view/", librarian_view, name="librarian_view"),
     path("member-view/", member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),      # ✅ add_book
-    path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),  # ✅ edit_book
-    path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),     
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"), 
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
