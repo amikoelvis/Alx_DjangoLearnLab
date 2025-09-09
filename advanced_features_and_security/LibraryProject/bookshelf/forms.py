755289cd20e566ad5be6ba308bf33f63f98d0061
Amiko Elvis <amikoelvis@gmail.com>
2025-09-09 17:05:43 +0300

    Uodate

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from .models import Book
 
+
+# Example form (not tied to a model)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Your Name")
+    email = forms.EmailField(label="Email Address")
+    message = forms.CharField(widget=forms.Textarea, label="Message")
+
+
+# Model form for Book (used in create/edit views)
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
