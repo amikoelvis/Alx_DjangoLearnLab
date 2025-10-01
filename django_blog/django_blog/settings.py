f7d2a523a8e357a76a7befd9c613855e54be8f3e
amikoelvis <amikoelvis@gmail.com>
2025-10-01 16:59:31 +0300

    Database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",   # or 'django.db.backends.sqlite3'
+        "NAME": "django_blog",                       # database name
+        "USER": "postgres",                          # your DB username
+        "PASSWORD": "your_password",                 # your DB password
+        "HOST": "localhost",                         # or "127.0.0.1"
+        "PORT": "5432",                              # default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
